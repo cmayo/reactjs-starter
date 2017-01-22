70030da024a3bbe240cc61ce1ac279c392b4b435
Carlos Mayo <cmayo@macbook.local>
2017-01-22 19:12:52 +0100

    Added publicPath output option

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 var path = require('path');
+var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    cache: true,
-    devtool: 'eval',
     entry: './src/app.js',
-    output:  {
-        path: path.join(__dirname, '/www/dist'),
+    output: {
+        path: path.join(__dirname, 'www/dist'),
+        publicPath: '/dist/',
         filename: 'app.js'
+    },
+    resolve: {
+		modulesDirectories: [
+			'node_modules'
+		]
     },
     module: {
         loaders: [
@@ -16,16 +21,33 @@ module.exports = {
                 exclude: /node_modules/,
                 loaders: ['react-hot', 'babel']
             },
-            { 
-                test: /\.less$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader")
+            {
+				test: /\.(less|css)$/,
+				exclude: /\b(some\-css\-framework|whatever)\b/i,
+				loader: ExtractTextPlugin.extract("style?sourceMap", "css?sourceMap!autoprefixer?browsers=last 2 version!less")
             }
         ]
     },
+    plugins: ([
+		// Avoid publishing files when compilation failed:
+		new webpack.NoErrorsPlugin(),
+
+		// Aggressively remove duplicate modules:
+		new webpack.optimize.DedupePlugin(),
+
+		// Write out CSS bundle to its own file:
+		new ExtractTextPlugin('style.css', { allChunks: true })
+	]).concat(process.env.WEBPACK_ENV==='dev' ? [] : [
+		new webpack.optimize.OccurenceOrderPlugin(),
+
+		// minify the JS bundle
+		new webpack.optimize.UglifyJsPlugin({
+			output: { comments: false },
+			exclude: [ /\.min\.js$/gi ]		// skip pre-minified libs
+		})
+    ]),
+    devtool: 'source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'www')
-    },
-    plugins: [
-        new ExtractTextPlugin("styles.css", {allChunks: true})
-    ]
+        contentBase: path.join(__dirname, 'www/')
+    }
 }
